efc51004edec86fcbccbf5381964babc43952cdf
emmy06.nar <emmy06.nar@gmail.com>
2025-07-30 11:26:55 +0530

    provided fix

--- .aws-sam/build/ProcessOrderFunction/app.py
+++ .aws-sam/build/ProcessOrderFunction/app.py
@@ -19,7 +19,7 @@ def lambda_handler(event, context):
             f"New order placed\n"
             f"Order ID : {order_id}\n"
             f"Customer : {customer}\n"
-            f"Amount   : {amount}"
+            f"Amount   : ₹{amount}"
         )
 
         sns.publish(
